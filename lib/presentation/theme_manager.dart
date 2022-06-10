ba5ca96cfa95a1d21a54dbe515dfeac3336c99b0
Wladimir Bandeira <tecnico100@gmail.com>
2022-06-10 14:31:25 -0300

    criando um gerenciador de temas e adicionando mais cores no gerenciador de cores

---
+++ lib/presentation/theme_manager.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+import 'package:register_image/presentation/color_manager.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+    // main colors of the app
+      primaryColor: ColorManager.primary,
+      primaryColorLight: ColorManager.primaryOpacity70,
+      primaryColorDark: ColorManager.darkPrimary,
+      disabledColor: ColorManager
+          .grey1, // will be used incase of disabled button for example
+      accentColor: ColorManager.grey
+    // card view theme
+
+    // App bar theme
+
+    // Button theme
+
+    // Text theme
+
+    // input decoration theme (text form field)
+
+  );
+}
