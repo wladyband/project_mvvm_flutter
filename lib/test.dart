b783670c2ef0fdd22ab93c55f7e7fc526fea1388
Wladimir Bandeira <tecnico100@gmail.com>
2022-06-10 14:11:18 -0300

    foi configurado a classe de fontes do projeto

---
+++ lib/test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:register_image/app/app.dart';
+
+class Test extends StatelessWidget {
+  const Test({Key? key}) : super(key: key);
+
+  void updateAppState(){
+    MyApp.instance.appState =10;
+  }
+  void getAppState(){
+    print( MyApp.instance.appState); // 10
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
