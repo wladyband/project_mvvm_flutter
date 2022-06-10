a3a5bb60545380246583979df31a77570551127e
Wladimir Bandeira <tecnico100@gmail.com>
2022-06-10 14:54:22 -0300

    criando estilo de botões no gerenciador de temas do projeto

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:register_image/presentation/color_manager.dart';
 import 'package:register_image/presentation/font_manager.dart';
@@ -8,33 +7,49 @@ import 'package:register_image/presentation/values_manager.dart';
 ThemeData getApplicationTheme() {
   return ThemeData(
     // main colors of the app
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.primaryOpacity70,
-      primaryColorDark: ColorManager.darkPrimary,
-      disabledColor: ColorManager.grey1,
-      // ripple color
-      splashColor: ColorManager.primaryOpacity70,
-      // will be used incase of disabled button for example
-      accentColor: ColorManager.grey,
-      // card view theme
-      cardTheme: CardTheme(
-          color: ColorManager.white,
-          shadowColor: ColorManager.grey,
-          elevation: AppSize.s4),
-      // App bar theme
-      appBarTheme: AppBarTheme(
-          centerTitle: true,
-          color: ColorManager.primary,
-          elevation: AppSize.s4,
-          shadowColor: ColorManager.primaryOpacity70,
-          titleTextStyle: getRegularStyle(
-              color: ColorManager.white, fontSize: FontSize.s16))
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.primaryOpacity70,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+    // ripple color
+    splashColor: ColorManager.primaryOpacity70,
+    // will be used incase of disabled button for example
+    accentColor: ColorManager.grey,
+    // card view theme
+    cardTheme: CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4),
+    // App bar theme
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.primaryOpacity70,
+      titleTextStyle:
+          getRegularStyle(color: ColorManager.white, fontSize: FontSize.s16),
+    ),
 
     // Button theme
+    buttonTheme: ButtonThemeData(
+        shape: StadiumBorder(),
+        disabledColor: ColorManager.grey1,
+        buttonColor: ColorManager.primary,
+        splashColor: ColorManager.primaryOpacity70),
+
+    // elevated button theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: getRegularStyle(color: ColorManager.white),
+        primary: ColorManager.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppSize.s12),
+        ),
+      ),
+    ),
 
     // Text theme
 
     // input decoration theme (text form field)
-
   );
 }
