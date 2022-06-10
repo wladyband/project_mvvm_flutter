95be08a2c84d239489fc5f2f29c21d5c78ebfaaf
Wladimir Bandeira <tecnico100@gmail.com>
2022-06-10 15:40:11 -0300

    finalizando gerenciador de Rotas

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,3 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:register_image/presentation/forgot_password/forgot_password.dart';
+import 'package:register_image/presentation/login/login.dart';
+import 'package:register_image/presentation/main/main_view.dart';
+import 'package:register_image/presentation/onboarding/onboarding.dart';
+import 'package:register_image/presentation/register/register.dart';
+import 'package:register_image/presentation/splash/splash.dart';
+import 'package:register_image/presentation/store_details/store_details.dart';
+
 class Routes {
   static const String splashRoute = "/";
   static const String onBoardingRoute = "/onBoarding";
@@ -6,4 +15,35 @@ class Routes {
   static const String forgotPasswordRoute = "/forgotPassword";
   static const String mainRoute = "/main";
   static const String storeDetailsRoute = "/storeDetails";
+}
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => SplashView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => LoginView());
+      case Routes.onBoardingRoute:
+        return MaterialPageRoute(builder: (_) => OnBoardingView());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => RegisterView());
+      case Routes.forgotPasswordRoute:
+        return MaterialPageRoute(builder: (_) => ForgotPasswordView());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => MainView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => StoreDetailsView());
+      default:
+        return unDefinedRoute();
+    }
+  }
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+        builder: (_) => Scaffold(
+          appBar: AppBar(
+            title: const Text("No Route Found"),
+          ),
+          body: const Center(child: Text("No Route Found")),
+        ));
+  }
 }
