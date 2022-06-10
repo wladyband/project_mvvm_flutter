f1b292f0934f0449bc3855aafccbc30dbcc425b8
Wladimir Bandeira <tecnico100@gmail.com>
2022-06-10 14:45:52 -0300

    criando estilo de Bar dentro do gerenciador de temas

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:register_image/presentation/color_manager.dart';
+import 'package:register_image/presentation/font_manager.dart';
+import 'package:register_image/presentation/styles_manager.dart';
 import 'package:register_image/presentation/values_manager.dart';
 
 ThemeData getApplicationTheme() {
@@ -9,16 +11,24 @@ ThemeData getApplicationTheme() {
       primaryColor: ColorManager.primary,
       primaryColorLight: ColorManager.primaryOpacity70,
       primaryColorDark: ColorManager.darkPrimary,
-      disabledColor: ColorManager
-          .grey1, // will be used incase of disabled button for example
+      disabledColor: ColorManager.grey1,
+      // ripple color
+      splashColor: ColorManager.primaryOpacity70,
+      // will be used incase of disabled button for example
       accentColor: ColorManager.grey,
       // card view theme
       cardTheme: CardTheme(
           color: ColorManager.white,
           shadowColor: ColorManager.grey,
-          elevation: AppSize.s4)
-
-    // App bar theme
+          elevation: AppSize.s4),
+      // App bar theme
+      appBarTheme: AppBarTheme(
+          centerTitle: true,
+          color: ColorManager.primary,
+          elevation: AppSize.s4,
+          shadowColor: ColorManager.primaryOpacity70,
+          titleTextStyle: getRegularStyle(
+              color: ColorManager.white, fontSize: FontSize.s16))
 
     // Button theme
 
