f4b7796e966b495a55a42e92a5dda2de6eb2a137
Wladimir Bandeira <tecnico100@gmail.com>
2022-06-10 15:29:23 -0300

    criando as paginas do projeto junto com um gerenciador de rotas

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:register_image/presentation/theme_manager.dart';
+import 'package:register_image/presentation/resources/theme_manager.dart';
 
 class MyApp extends StatefulWidget {
   MyApp._internal(); // private named constructor
--- lib/presentation/resources/theme_manager.dart
+++ lib/presentation/resources/theme_manager.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:register_image/presentation/color_manager.dart';
-import 'package:register_image/presentation/font_manager.dart';
-import 'package:register_image/presentation/styles_manager.dart';
-import 'package:register_image/presentation/values_manager.dart';
+import 'package:register_image/presentation/resources/color_manager.dart';
+import 'package:register_image/presentation/resources/font_manager.dart';
+import 'package:register_image/presentation/resources/styles_manager.dart';
+import 'package:register_image/presentation/resources/values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
@@ -30,7 +30,7 @@ ThemeData getApplicationTheme() {
               color: ColorManager.white, fontSize: FontSize.s16)),
       // Button theme
       buttonTheme: ButtonThemeData(
-          shape: StadiumBorder(),
+          shape: const StadiumBorder(),
           disabledColor: ColorManager.grey1,
           buttonColor: ColorManager.primary,
           splashColor: ColorManager.primaryOpacity70),
@@ -54,7 +54,7 @@ ThemeData getApplicationTheme() {
       // input decoration theme (text form field)
 
       inputDecorationTheme: InputDecorationTheme(
-        contentPadding: EdgeInsets.all(AppPadding.p8),
+        contentPadding: const EdgeInsets.all(AppPadding.p8),
         // hint style
         hintStyle: getRegularStyle(color: ColorManager.grey1),
 
@@ -67,23 +67,23 @@ ThemeData getApplicationTheme() {
         enabledBorder: OutlineInputBorder(
             borderSide:
                 BorderSide(color: ColorManager.grey, width: AppSize.s1_5),
-            borderRadius: BorderRadius.all(Radius.circular(AppSize.s8))),
+            borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8))),
 
         // focused border
         focusedBorder: OutlineInputBorder(
             borderSide:
                 BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
-            borderRadius: BorderRadius.all(Radius.circular(AppSize.s8))),
+            borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8))),
 
         // error border
         errorBorder: OutlineInputBorder(
             borderSide:
                 BorderSide(color: ColorManager.error, width: AppSize.s1_5),
-            borderRadius: BorderRadius.all(Radius.circular(AppSize.s8))),
+            borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8))),
         // focused error border
         focusedErrorBorder: OutlineInputBorder(
             borderSide:
                 BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
-            borderRadius: BorderRadius.all(Radius.circular(AppSize.s8))),
+            borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8))),
       ));
 }
