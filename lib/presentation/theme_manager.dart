12ec303291a433ef91218296c4fb48563f13e4ea
Wladimir Bandeira <tecnico100@gmail.com>
2022-06-10 14:40:03 -0300

    adicionando um estilo de Card no gerenciador de temas

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:register_image/presentation/color_manager.dart';
+import 'package:register_image/presentation/values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
@@ -10,8 +11,12 @@ ThemeData getApplicationTheme() {
       primaryColorDark: ColorManager.darkPrimary,
       disabledColor: ColorManager
           .grey1, // will be used incase of disabled button for example
-      accentColor: ColorManager.grey
-    // card view theme
+      accentColor: ColorManager.grey,
+      // card view theme
+      cardTheme: CardTheme(
+          color: ColorManager.white,
+          shadowColor: ColorManager.grey,
+          elevation: AppSize.s4)
 
     // App bar theme
 
