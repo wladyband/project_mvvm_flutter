35370dbb1ee255a0eb9e54195dd33d64385ab8e3
Wladimir Bandeira <tecnico100@gmail.com>
2022-06-12 15:41:00 -0300

    adicionando temporarizador de tela inicial

--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:register_image/presentation/resources/assets_manager.dart';
 import 'package:register_image/presentation/resources/color_manager.dart';
+import 'package:register_image/presentation/resources/routes_manager.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({Key? key}) : super(key: key);
@@ -10,6 +12,30 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+
+  Timer? _timer;
+
+  _startDelay() {
+    _timer = Timer(Duration(seconds: 2), _goNext);
+  }
+
+  _goNext() {
+    Navigator.pushReplacementNamed(context, Routes.onBoardingRoute);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startDelay();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
