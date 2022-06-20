046008c0dd0c6f1785cc0c0266f7832f39a2989d
Wladimir Bandeira <tecnico100@gmail.com>
2022-06-20 10:49:28 -0300

    adicionando o Repository, Request e Failure

---
+++ lib/domain/repository.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:register_image/data/network/failure.dart';
+import 'package:register_image/data/request/request.dart';
+
+import 'model.dart';
+
+abstract class Repository{
+  Future<Either<Failure,Authentication>> login(LoginRequest loginRequest);
+}
