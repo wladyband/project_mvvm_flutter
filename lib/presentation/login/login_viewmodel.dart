28d0f68935a295d3dfc520224d34e1294e613f47
Wladimir Bandeira <tecnico100@gmail.com>
2022-06-27 10:29:21 -0300

    adicionar  Streams no Login ViewModel

---
+++ lib/presentation/login/login_viewmodel.dart
@@ -0,0 +1,85 @@
+import 'dart:async';
+import '/presentation/base/baseviewmodel.dart';
+
+class LoginViewModel extends BaseViewModel
+    with LoginViewModelInputs, LoginViewModelOutputs {
+  StreamController _userNameStreamController =
+      StreamController<String>.broadcast();
+  StreamController _passwordStreamController =
+      StreamController<String>.broadcast();
+
+  // inputs
+  @override
+  void dispose() {
+    _userNameStreamController.close();
+    _passwordStreamController.close();
+  }
+
+  @override
+  void start() {
+    // TODO: implement start
+  }
+
+  @override
+  Sink get inputPassword => _passwordStreamController.sink;
+
+  @override
+  Sink get inputUserName => _userNameStreamController.sink;
+
+  @override
+  login() {
+    // TODO: implement login
+    throw UnimplementedError();
+  }
+
+  @override
+  setPassword(String password) {
+    // TODO: implement setPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  setUserName(String userName) {
+    // TODO: implement setUserName
+    throw UnimplementedError();
+  }
+
+  // outputs
+  @override
+  Stream<bool> get outputIsPasswordValid => _passwordStreamController.stream
+      .map((password) => _isPasswordValid(password));
+
+  @override
+  Stream<bool> get outputIsUserNameValid => _userNameStreamController.stream
+      .map((userName) => _isUserNameValid(userName));
+
+  // private functions
+
+  bool _isPasswordValid(String password) {
+    return password.isNotEmpty;
+  }
+
+  bool _isUserNameValid(String userName) {
+    return userName.isNotEmpty;
+  }
+}
+
+abstract class LoginViewModelInputs {
+  // three functions for actions
+  setUserName(String userName);
+
+  setPassword(String password);
+
+  login();
+
+// two sinks for streams
+  Sink get inputUserName;
+
+  Sink get inputPassword;
+}
+
+abstract class LoginViewModelOutputs {
+  Stream<bool> get outputIsUserNameValid;
+
+  Stream<bool> get outputIsPasswordValid;
+}
