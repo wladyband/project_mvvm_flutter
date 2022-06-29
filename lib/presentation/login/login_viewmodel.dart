09612ee6a4bb5091ac3b9302466afe9d6e68d65a
Wladimir Bandeira <tecnico100@gmail.com>
2022-06-29 11:30:23 -0300

    adicionar no Login UseCase para  Login ViewModel

--- lib/presentation/login/login_viewmodel.dart
+++ lib/presentation/login/login_viewmodel.dart
@@ -1,17 +1,20 @@
 import 'dart:async';
-import 'package:register_image/presentation/common/freezed_data_classes.dart';
-
+import '/domain/usecase/login_usecase.dart';
 import '/presentation/base/baseviewmodel.dart';
+import '/presentation/common/freezed_data_classes.dart';
 
 class LoginViewModel extends BaseViewModel
     with LoginViewModelInputs, LoginViewModelOutputs {
   StreamController _userNameStreamController =
-      StreamController<String>.broadcast();
+  StreamController<String>.broadcast();
   StreamController _passwordStreamController =
-      StreamController<String>.broadcast();
+  StreamController<String>.broadcast();
 
+  var loginObject = LoginObject("", "");
 
-  var loginObject = LoginObject("","");
+  LoginUseCase _loginUseCase;
+
+  LoginViewModel(this._loginUseCase);
 
   // inputs
   @override
@@ -32,21 +35,32 @@ class LoginViewModel extends BaseViewModel
   Sink get inputUserName => _userNameStreamController.sink;
 
   @override
-  login() {
-    // TODO: implement login
-    throw UnimplementedError();
+  login() async {
+    (await _loginUseCase.execute(
+        LoginUseCaseInput(loginObject.userName, loginObject.password)))
+        .fold(
+            (failure) => {
+          // left -> failure
+          print(failure.message)
+        },
+            (data) => {
+          // right -> success (data)
+          print(data.customer?.name)
+        });
   }
 
   @override
   setPassword(String password) {
-    // TODO: implement setPassword
-    throw UnimplementedError();
+    inputPassword.add(password);
+    loginObject = loginObject.copyWith(
+        password: password); // data class operation same as kotlin
   }
 
   @override
   setUserName(String userName) {
-    // TODO: implement setUserName
-    throw UnimplementedError();
+    inputUserName.add(userName);
+    loginObject = loginObject.copyWith(
+        userName: userName); // data class operation same as kotlin
   }
 
   // outputs
