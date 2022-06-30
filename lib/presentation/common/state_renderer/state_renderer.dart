bebbe782cc810f7b8f0da96d7553a3b141c417a2
Wladimir Bandeira <tecnico100@gmail.com>
2022-06-30 15:46:04 -0300

    adicionando implementação de render state

---
+++ lib/presentation/common/state_renderer/state_renderer.dart
@@ -0,0 +1,41 @@
+import '/data/mapper/mapper.dart';
+import '/data/network/failure.dart';
+import '/presentation/resources/strings_manager.dart';
+import 'package:flutter/material.dart';
+
+enum StateRendererType {
+  // POPUP STATES
+  POPUP_LOADING_STATE,
+  POPUP_ERROR_STATE,
+
+  // FULL SCREEN STATES
+  FULL_SCREEN_LOADING_STATE,
+  FULL_SCREEN_ERROR_STATE,
+  CONTENT_SCREEN_STATE, // THE UI OF THE SCREEN
+  EMPTY_SCREEN_STATE // EMPTY VIEW WHEN WE RECEIVE NO DATA FROM API SIDE FOR LIST SCREEN
+}
+
+class StateRenderer extends StatelessWidget {
+  StateRendererType stateRendererType;
+  Failure failure;
+  String message;
+  String title;
+  Function retryActionFunction;
+
+  StateRenderer(
+      {Key? key,
+        required this.stateRendererType,
+        Failure? failure,
+        String? message,
+        String? title,
+        required this.retryActionFunction})
+      : message = message ?? AppStrings.loading,
+        title = title ?? EMPTY,
+        failure = failure ?? DefaultFailure(),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
